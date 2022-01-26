5b1ae75b04f89f50f44e3916a584dac443317951
username <nomadicalli@mail.ru>
2022-01-26 13:10:51 +0300

    Release 5

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 const express = require('express');
+const { get } = require('http');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.json('World')
+    res.send('Hello world')
 });
 
 app.get('/users', (req, res) => {
-    res.json(["user1", "user2", "user3"])
+    res.json([
+        "user1",
+        "user2",
+        "user3"
+    ])
 });
 
 app.post('/users', (req, res) => {
-    res.send("Юзер добавлен")
+    res.send("юзер добавлен")
 });
 
-app.delete('/admin', (req, res) => {
+app.delete('/users/:id', (req, res) => {
+    res.send(`юзер добавлен с ID :${req.params.id} удален`)
+});
+
+app.patch('/users/:id', (req, res) => {
+    res.send(`юзер с ID :${req.params.id}`)
+});
+
+app.get('/admin', (req, res) => {
     res.send("не хватает прав доступа")
-})
+});
+
+
 
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`)
+    console.log(`localhost:${port}`)
 })
