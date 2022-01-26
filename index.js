a7cf4b457f713e9b3e0e299e5f3c5f6adefd2ab0
username <nomadicalli@mail.ru>
2022-01-26 11:57:41 +0300

    release 4

--- index.js
+++ index.js
@@ -2,26 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  });
-  
-
-
-app.get('/city', (req, res) => {
-    res.json([
-        'Grozny',
-        'Casablanca',
-        'Cosovo'
-    ]
-    )
+    res.json('World')
 });
 
+app.get('/users', (req, res) => {
+    res.json(["user1", "user2", "user3"])
+});
 
+app.post('/users', (req, res) => {
+    res.send("Юзер добавлен")
+});
 
-
+app.delete('/admin', (req, res) => {
+    res.send("не хватает прав доступа")
+})
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`http://localhost:${port}`)
 })
